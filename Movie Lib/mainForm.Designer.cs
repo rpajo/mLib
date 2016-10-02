e3c6ff0fd14bfce18708536225d0c54c571a055c
Rok <rok.pajnic@gmail.com>
2016-10-02 23:58:54 +0200

    Load movie posters

--- Movie Lib/mainForm.Designer.cs
+++ Movie Lib/mainForm.Designer.cs
@@ -30,6 +30,10 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.moviePanel = new System.Windows.Forms.TableLayoutPanel();
+            this.libRefresh = new MetroFramework.Controls.MetroButton();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.dirLink = new MetroFramework.Controls.MetroLink();
+            this.chDir = new MetroFramework.Controls.MetroButton();
             this.SuspendLayout();
             // 
             // moviePanel
@@ -38,12 +42,41 @@
             this.moviePanel.Name = "moviePanel";
             this.moviePanel.Paint += new System.Windows.Forms.PaintEventHandler(this.moviePanel_Paint);
             // 
+            // libRefresh
+            // 
+            resources.ApplyResources(this.libRefresh, "libRefresh");
+            this.libRefresh.Name = "libRefresh";
+            this.libRefresh.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroLabel1
+            // 
+            resources.ApplyResources(this.metroLabel1, "metroLabel1");
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Click += new System.EventHandler(this.metroLabel1_Click_1);
+            // 
+            // dirLink
+            // 
+            resources.ApplyResources(this.dirLink, "dirLink");
+            this.dirLink.Name = "dirLink";
+            this.dirLink.Click += new System.EventHandler(this.dirLink_Click);
+            // 
+            // chDir
+            // 
+            resources.ApplyResources(this.chDir, "chDir");
+            this.chDir.Name = "chDir";
+            this.chDir.Click += new System.EventHandler(this.chDir_Click);
+            // 
             // MainForm
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.chDir);
+            this.Controls.Add(this.dirLink);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.libRefresh);
             this.Controls.Add(this.moviePanel);
             this.Name = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -52,6 +85,10 @@
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel moviePanel;
+        private MetroFramework.Controls.MetroButton libRefresh;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLink dirLink;
+        private MetroFramework.Controls.MetroButton chDir;
     }
 }
 
--- Movie Lib/mainForm.cs
+++ Movie Lib/mainForm.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Movie_Lib
@@ -15,14 +11,30 @@ namespace Movie_Lib
         public MainForm()
         {
             InitializeComponent();
-            List<Movie> movies = Movie_Lib.Program.GetFiles(@"D:/Downloads");
+            dirLink.Text = "D:/Downloads";
+        }
 
-            int index = 0;
-            foreach (Movie movie in movies)
+
+        private void addMovie(Movie movie)
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            try
             {
-                addMovie(movie);
-                index++;
+                movieControl movieFrame = new movieControl();
+                String title = movie.name != null ? movie.name : movie.dir.Name;
+                movieFrame.Controls[1].Text = title;
+                Console.WriteLine(movieFrame.Controls[0].Name);
+                PictureBox posterBox = (PictureBox)movieFrame.Controls[0];
+                posterBox.ImageLocation = movie.posterUrl;
+                posterBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                moviePanel.Controls.Add(movieFrame);
+                resources.ApplyResources(movieFrame, movie.dir.Name);
             }
+            catch
+            {
+
+            }
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -40,22 +52,7 @@ namespace Movie_Lib
 
         }
 
-        private void addMovie( Movie movie)
-        {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-            try {
-                movieControl movieFrame = new movieControl();
-                String title = movie.name != null ? movie.name : movie.dir.Name;
-                movieFrame.Controls[1].Text = title;
-                moviePanel.Controls.Add(movieFrame);
-                resources.ApplyResources(movieFrame, movie.dir.Name);
-            }
-            catch
-            {
 
-            }
-            
-        }
 
         private void metroTile1_Click_1(object sender, EventArgs e)
         {
@@ -63,6 +60,48 @@ namespace Movie_Lib
         }
 
         private void moviePanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            moviePanel.Controls.Clear();
+            List<Movie> movies = Movie_Lib.Program.GetFiles(dirLink.Text);
+
+            foreach (Movie movie in movies)
+            {
+                addMovie(movie);
+            }
+            Console.WriteLine("DONE!");
+        }
+
+        private void metroLabel1_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void dirLink_Click(object sender, EventArgs e)
+        {
+            Process.Start(dirLink.Text);
+        }
+
+        private void chDir_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            DialogResult result = fbd.ShowDialog();
+
+            if (!string.IsNullOrWhiteSpace(fbd.SelectedPath))
+            {
+                string[] files = Directory.GetFiles(fbd.SelectedPath);
+
+                //System.Windows.Forms.MessageBox.Show("Dir selected: " + fbd.SelectedPath);
+                dirLink.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
         {
 
         }
